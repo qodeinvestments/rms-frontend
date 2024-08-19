f324afe6f1b06a5a1570d8146da5718f7b5e9d4c
Abhinav <abhinav.rajhans1@gmail.com>
2024-08-19 16:13:30 +0530

    correct error to errorLogs

--- src/components/Errors.vue
+++ src/components/Errors.vue
@@ -93,7 +93,7 @@ const handleMessage = (message) => {
 }
 
 const connectToSSE = () => {
-    const socket = new WebSocket('wss://api.swancapital.in/errors');
+    const socket = new WebSocket('wss://api.swancapital.in/errorLogs');
 
     socket.onmessage = (event) => {
         if (event.data === 'ping') {
