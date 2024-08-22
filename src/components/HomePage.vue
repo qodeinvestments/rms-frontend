47ac178c75dce308403325d4c9e0ca9e63491916
Abhinav <abhinav.rajhans1@gmail.com>
2024-08-22 09:10:59 +0530

    updated index data

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -152,11 +152,11 @@ const basket_BackendData = ref({})
 const strategy_mtm_chart_BackendData = ref({})
 const index_data = ref({})
 const previous_day_close_index_data = {
-  BANKNIFTYSPOT: 50803.15,
-  FINNIFTYSPOT: 23178.15,
-  MIDCPNIFTYSPOT: 12868.70,
-  NIFTYSPOT: 24698.85,
-  SENSEXSPOT: 80802.86
+  BANKNIFTYSPOT: 50685.55,
+  FINNIFTYSPOT: 23143.65,
+  MIDCPNIFTYSPOT: 12938.15,
+  NIFTYSPOT: 24770.20,
+  SENSEXSPOT: 80905.30
 }
 const pulse_signal = ref([])
 const time = ref([])
