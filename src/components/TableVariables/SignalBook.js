c165d26fe6e1874774860a499033d4af9e55364c
abhina <abhinav.rajhans1@gmail.com>
2025-01-03 13:26:59 +0530

    corrected the columns format of signal book

--- src/components/TableVariables/SignalBook.js
+++ src/components/TableVariables/SignalBook.js
@@ -8,10 +8,10 @@ import {
 const columnHelper = createColumnHelper()
 
  
-const columns_signal_book = ['trade_id', 'uid', 'timestamp', 'system_timestamp',
-     'system_tag', 'action', 'action_int', 'qty', 
-     'qty_dir', 'symbol', 'price', 'price_provided',
-      'value', 'buy_value', 'sell_value','note', 'quantity'
+const columns_signal_book = [ 'timestamp', 'system_timestamp',
+     'system_tag', 'action',  'symbol',  'price','qty','quantity','trade_id', 'uid',
+     'qty_dir', , 'price_provided',
+      'value', 'buy_value', 'sell_value','note', 'action_int'
 ];
 
 export const columns  = columns_signal_book.map(column => {
