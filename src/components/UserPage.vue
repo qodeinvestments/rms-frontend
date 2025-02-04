09ca84e06527988eae63087c065833997062017f
abhina <abhinav.rajhans1@gmail.com>
2025-02-04 16:18:25 +0530

    added EOD book in userpage

--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -29,7 +29,7 @@ const basket = ref([])
 import { live_trade_book_columns_zerodha,live_trade_book_columns_xts,live_order_book_columns_zerodha
   ,fund_summary_columns,live_order_book_columns_xts,signal_position,columns,combined_df_columns_zerodha,
   combined_df_columns_xts,rms_df_columns,combined_order_zerodha,combined_order_xts,combined_trades_zerodha,
-  curr_strategy_mtm,curr_basket_mtm,combined_trades_xts,zerodha_order_book_columns,holding_book_columns,zerodha_position_book_columns
+  curr_strategy_mtm,curr_basket_mtm,combined_trades_xts,zerodha_order_book_columns,holding_book_columns,zerodha_position_book_columns,rms_prev_day
  } from '../components/TableVariables/UserPageTable.js'; 
 
 
@@ -488,7 +488,7 @@ watch(selectedBasketItems, (newSelectedBasketItems) => {
     </div>
     <div class="navContainer">
       <NavBar
-        :navColumns="['Positions', 'Order', 'Combined DF', 'Combined Orders', 'Combined Trades', 'Fund Summary','Zerodha Order Book','Zerodha Position Book','Holdings']"
+        :navColumns="['Positions', 'Order', 'Combined DF', 'Combined Orders', 'Combined Trades', 'Fund Summary','Zerodha Order Book','Zerodha Position Book','Holdings','EOD']"
         @column-clicked="handleColumnClick" :colorColumns="[]" />
     </div>
     <div class="selectContainer" v-if="book && showOnPage === 'Combined DF' && filteredSignalBookData.length">
@@ -534,6 +534,11 @@ watch(selectedBasketItems, (newSelectedBasketItems) => {
     <div class="my-8" v-if="book && showOnPage === 'Holdings'">
       <TanStackTestTable title="Holdings" :data="book" :columns="holding_book_columns"
         :hasColor="[]" :navigateTo="[]" :showPagination=true />
+    </div>
+
+    <div class="my-8" v-if="book && showOnPage === 'EOD'">
+      <TanStackTestTable title="EOD" :data="book" :columns="rms_prev_day"
+        :hasColor="['Quantity']" :navigateTo="[]" :showPagination=true />
     </div>
 
     <div class="my-8" v-if="book && showOnPage === 'Combined DF' && filteredSignalBookData.length">
