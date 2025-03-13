fc492f67d676f3f69691be173121255e085fcb57
abhina <abhinav.rajhans1@gmail.com>
2025-03-13 23:47:04 +0530

    added cgabf

--- src/components/SlippageUserPage.vue
+++ src/components/SlippageUserPage.vue
@@ -53,7 +53,7 @@
                 </th>
               </tr>
               <!-- Header Row (clickable for sorting) -->
-              <tr class="header-row">
+              <tr class="sum-row">
                 <th
                   v-for="column in allColumns"
                   :key="'header-' + column"
