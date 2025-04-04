7cfce7ff6c60eff2d740eb384fa261b524839dc0
abhina <abhinav.rajhans1@gmail.com>
2025-04-04 14:05:28 +0530

    increase margin

--- src/components/VarSimulator.vue
+++ src/components/VarSimulator.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div class="max-w-3xl mx-auto bg-white rounded-lg shadow-lg p-8">
+      <div class="max-w-7xl mx-auto bg-white rounded-lg shadow-lg p-8">
         <h1 class="text-3xl font-bold text-center text-gray-800 mb-8">
           User Trade Management
         </h1>
