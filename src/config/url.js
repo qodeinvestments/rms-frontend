8fa2219b50cdd70010d1b7c89c3fbe659fb350be
abhinavqode <abhinav.rajhans@qodeinvest.com>
2025-06-23 16:35:01 +0530

    added .env and updated .gitignore

--- src/config/url.js
+++ src/config/url.js
@@ -1,4 +1,2 @@
-// src/config.js
-
-export const API_BASE_URL = 'https://production2.swancapital.in/';
-export const WS_BASE_URL = 'wss://production2.swancapital.in/';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const WS_BASE_URL = import.meta.env.VITE_WS_BASE_URL;
