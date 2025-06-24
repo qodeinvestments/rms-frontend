8ee7c691c1cc85c5d3c203c011ad9ff0ebc704f4
abhinavqode <abhinav.rajhans@qodeinvest.com>
2025-06-24 15:20:13 +0530

    removed the .env

--- src/config/url.js
+++ src/config/url.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const WS_BASE_URL = import.meta.env.VITE_WS_BASE_URL;
+export const API_BASE_URL =  'https://api.swancapital.in/';
+export const WS_BASE_URL =  'wss://api.swancapital.in/';
